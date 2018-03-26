248ab19a4d3b48b89fe8cf2d91437b2af7caa527
B85M <jie.jin@wxbester.cn>
2018-03-26 12:38:34 +0800

    Initial Commit

---
+++ jumptest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jumptest
+//
+//  Created by pi on 26/03/2018.
+//  Copyright © 2018 wxbester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
