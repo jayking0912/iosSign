7cb5814c9ffb55c6b3c11674d1a874b684fd5d33
B150MD3H <jayking0912@foxmail.com>
2018-04-03 21:08:16 +0800

    no message

---
+++ OneQianDao/OneQianDao/ViewController/CardLayoutVC.h
@@ -0,0 +1,16 @@
+//
+//  CardLayoutVC.h
+//  AwsomeLayout
+//
+//  Created by Mr.Chen on 2017/3/12.
+//  Copyright © 2017年 陈凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define screen_H [UIScreen mainScreen].bounds.size.height
+#define screen_W [UIScreen mainScreen].bounds.size.width
+
+@interface CardLayoutVC : UIViewController
+
+@end
--- OneQianDao/OneQianDao/ViewController/CardLayoutVC.m
+++ OneQianDao/OneQianDao/ViewController/CardLayoutVC.m
@@ -1,22 +1,21 @@
 //
-//  ViewController.m
-//  OneQianDao
+//  CardLayoutVC.m
+//  AwsomeLayout
 //
-//  Created by pi on 01/04/2018.
-//  Copyright © 2018 jayking. All rights reserved.
+//  Created by Mr.Chen on 2017/3/12.
+//  Copyright © 2017年 陈凯. All rights reserved.
 //
 
-#import "ViewController.h"
 #import "CardLayoutVC.h"
 #import "CardLayOut.h"
 
-@interface ViewController ()<UICollectionViewDataSource, UICollectionViewDelegate>
+@interface CardLayoutVC () <UICollectionViewDataSource, UICollectionViewDelegate>
 
 @property (strong, nonatomic) UICollectionView *collectionView;
 
 @end
 
-@implementation ViewController
+@implementation CardLayoutVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -25,7 +24,7 @@
     
     CardLayOut *layout = [CardLayOut new];
     layout.scale = 1.1;
-    //    layout.itemSize = CGSizeMake(200, 300);
+//    layout.itemSize = CGSizeMake(200, 300);
     layout.itemSize = CGSizeMake(screen_W-40, 350);
     layout.scrollDirection = UICollectionViewScrollDirectionVertical;
     self.collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
@@ -35,8 +34,8 @@
     self.collectionView.showsVerticalScrollIndicator = NO;
     //背景底色
     self.collectionView.backgroundColor = [UIColor colorWithWhite:0.95 alpha:1];
-    //    self.collectionView.backgroundColor = [UIColor colorWithRed:0.5 green:0.6 blue:0.8 alpha:1.0];
-    //    self.collectionView.tintColor = [UIColor blueColor];
+//    self.collectionView.backgroundColor = [UIColor colorWithRed:0.5 green:0.6 blue:0.8 alpha:1.0];
+//    self.collectionView.tintColor = [UIColor blueColor];
     [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"CellID"];
     [self.view addSubview:self.collectionView];
 }
@@ -52,7 +51,7 @@
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"CellID" forIndexPath:indexPath];
-    //    cell.backgroundColor = [UIColor colorWithWhite:0.8 alpha:1];
+//    cell.backgroundColor = [UIColor colorWithWhite:0.8 alpha:1];
     cell.backgroundColor = [UIColor redColor]; //每个cell颜色
     
     cell.layer.cornerRadius = 5;
